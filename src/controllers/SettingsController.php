391a0214c16f88f70c1045a97ea4cdbf189b4945
Iwona Just <iwona@pixelandtonic.com>
2024-04-04 08:36:09 +0100

    plugin settings

---
+++ src/controllers/SettingsController.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\stripe\controllers;
+
+use Craft;
+use craft\helpers\StringHelper;
+use craft\queue\jobs\ResaveElements;
+use craft\stripe\elements\Product;
+use craft\stripe\models\Settings;
+use craft\stripe\Plugin;
+use craft\web\Controller;
+use yii\web\Response;
+
+/**
+ * The SettingsController handles modifying and saving the general settings.
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ * @since 3.0
+ */
+class SettingsController extends Controller
+{
+    /**
+     * Display a form to allow an administrator to update plugin settings.
+     *
+     * @return Response
+     */
+    public function actionIndex(?Settings $settings = null): Response
+    {
+        if ($settings == null) {
+            $settings = Plugin::getInstance()->getSettings();
+        }
+
+        $tabs = [
+            'apiConnection' => [
+                'label' => Craft::t('stripe', 'API Connection'),
+                'url' => '#api',
+            ],
+            'products' => [
+                'label' => Craft::t('stripe', 'Products'),
+                'url' => '#products',
+            ],
+        ];
+
+        return $this->renderTemplate('stripe/settings/index', compact('settings', 'tabs'));
+    }
+
+    /**
+     * Save the settings.
+     *
+     * @return ?Response
+     */
+    public function actionSaveSettings(): ?Response
+    {
+        $settings = Craft::$app->getRequest()->getParam('settings');
+        $plugin = Plugin::getInstance();
+        /** @var Settings $pluginSettings */
+        $pluginSettings = $plugin->getSettings();
+//        $originalUriFormat = $pluginSettings->uriFormat;
+//
+//        // Remove from editable table namespace
+//        $settings['uriFormat'] = $settings['routing']['uriFormat'];
+//        // Could be blank if in headless mode
+//        if (isset($settings['routing']['template'])) {
+//            $settings['template'] = $settings['routing']['template'];
+//        }
+//        unset($settings['routing']);
+
+        $settingsSuccess = Craft::$app->getPlugins()->savePluginSettings($plugin, $settings);
+
+//        $fieldLayout = Craft::$app->getFields()->assembleLayoutFromPost();
+//        $fieldLayout->type = Product::class;
+//
+//        $projectConfig = Craft::$app->getProjectConfig();
+//        $uid = StringHelper::UUID();
+//        $fieldLayoutConfig = $fieldLayout->getConfig();
+//        $projectConfig->set(Plugin::PC_PATH_PRODUCT_FIELD_LAYOUTS, [$uid => $fieldLayoutConfig], 'Save the Shopify product field layout');
+//
+//        $pluginSettings->setProductFieldLayout($fieldLayout);
+
+        if (!$settingsSuccess) {
+            return $this->asModelFailure(
+                $pluginSettings,
+                Craft::t('stripe', 'Couldn’t save settings.'),
+                'settings',
+            );
+        }
+
+//        // Resave all products if the URI format changed
+//        if ($originalUriFormat != $settings['uriFormat']) {
+//            Craft::$app->getQueue()->push(new ResaveElements([
+//                'elementType' => Product::class,
+//                'criteria' => [
+//                    'siteId' => '*',
+//                    'unique' => true,
+//                    'status' => null,
+//                ],
+//            ]));
+//        }
+
+        return $this->asModelSuccess(
+            $pluginSettings,
+            Craft::t('stripe', 'Settings saved.'),
+            'settings',
+        );
+    }
+}
--- src/models/Settings.php
+++ src/models/Settings.php
@@ -7,7 +7,32 @@ use craft\base\Model;
 
 /**
  * Stripe settings
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
  */
 class Settings extends Model
 {
+    public string $secretKey = '';
+    public string $publicKey = '';
+
+    /**
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return [
+            [['secretKey', 'publicKey'], 'required'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels(): array
+    {
+        return [
+            'secretKey' => Craft::t('stripe', 'Stripe Secret Key'),
+            'publicKey' => Craft::t('stripe', 'Stripe Public Key'),
+        ];
+    }
 }
