d61ca502dee9d35938dc6a72da655dd15a8055e4
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-04-25 11:17:08 +0100

    Add `PriceCondtion` and `PriceTypeConditionRule`

---
+++ src/elements/conditions/products/PriceCondition.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\stripe\elements\conditions\products;
+
+use craft\elements\conditions\ElementCondition;
+use craft\errors\InvalidTypeException;
+
+/**
+ * Price condition
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ */
+class PriceCondition extends ElementCondition
+{
+    /**
+     * @throws InvalidTypeException
+     */
+    protected function selectableConditionRules(): array
+    {
+        return array_merge(parent::selectableConditionRules(), [
+            PriceTypeConditionRule::class,
+        ]);
+    }
+}
---
+++ src/elements/conditions/products/PriceTypeConditionRule.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @link https://craftcms.com/
+ * @copyright Copyright (c) Pixel & Tonic, Inc.
+ * @license https://craftcms.github.io/license/
+ */
+
+namespace craft\stripe\elements\conditions\products;
+
+use craft\base\conditions\BaseMultiSelectConditionRule;
+use craft\base\ElementInterface;
+use craft\elements\conditions\ElementConditionRuleInterface;
+use craft\elements\db\ElementQueryInterface;
+use craft\helpers\StringHelper;
+use craft\stripe\elements\db\PriceQuery;
+use craft\stripe\elements\Price;
+use craft\stripe\enums\PriceType;
+
+/**
+ * Class PriceTypeConditionRule
+ *
+ * @author Pixel & Tonic, Inc. <support@pixelandtonic.com>
+ */
+class PriceTypeConditionRule extends BaseMultiSelectConditionRule implements ElementConditionRuleInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return \Craft::t('stripe', 'Type');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function options(): array
+    {
+        return array_map(function($option) {
+            return [
+                'value' => $option,
+                'label' => StringHelper::humanize($option),
+            ];
+        }, [
+            PriceType::OneTime->value,
+            PriceType::Recurring->value,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getExclusiveQueryParams(): array
+    {
+        return ['type'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function matchElement(ElementInterface $element): bool
+    {
+        /** @var Price $element */
+        return $this->matchValue($element->priceType);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function modifyQuery(ElementQueryInterface $query): void
+    {
+        /** @var PriceQuery $query */
+        $query->priceType($this->paramValue());
+    }
+}
