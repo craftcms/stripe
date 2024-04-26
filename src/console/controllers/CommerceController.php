206cb850a8e13d04bf4c0a755dc5f0645ebd942c
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2024-04-26 13:53:09 +0100

    Fix Commerce subscription migration

--- src/console/controllers/CommerceController.php
+++ src/console/controllers/CommerceController.php
@@ -8,11 +8,15 @@
 namespace craft\stripe\console\controllers;
 
 use Craft;
+use craft\commerce\base\Gateway;
 use craft\commerce\elements\Subscription as CommerceSubscription;
+use craft\commerce\Plugin as CommercePlugin;
 use craft\console\Controller;
+use craft\errors\DeprecationException;
 use craft\helpers\Console;
 use craft\helpers\StringHelper;
 use craft\stripe\elements\Subscription;
+use yii\base\InvalidConfigException;
 use yii\console\ExitCode;
 
 /**
@@ -35,6 +39,11 @@ class CommerceController extends Controller
             return ExitCode::UNSPECIFIED_ERROR;
         }
 
+        if (empty($this->_stripeCommerceGatewayIds())) {
+            $this->stderr("No Stripe Commerce gateways found.\n", Console::FG_RED);
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+
         $this->stdout('Starting to migrate data from Craft Commerce…' . PHP_EOL . PHP_EOL, Console::FG_GREEN);
 
         $fieldsService = Craft::$app->getFields();
@@ -52,26 +61,52 @@ class CommerceController extends Controller
         }
 
         // migrate subscriptions
-        $this->migrateSubscriptionData();
+        $this->_migrateSubscriptionData();
 
-        $this->stdout('Finished migrating data from Craft Commerce…' . PHP_EOL . PHP_EOL, Console::FG_GREEN);
+        $this->stdout('Finished migrating data from Craft Commerce. Please run the stripe/sync/all command…' . PHP_EOL . PHP_EOL, Console::FG_GREEN);
 
         return ExitCode::OK;
     }
 
-    private function migrateSubscriptionData(): void
+    /**
+     * @return array
+     * @throws DeprecationException
+     * @throws InvalidConfigException
+     */
+    private function _stripeCommerceGatewayIds(): array
     {
+        // Get all Stripe gateways
+        $gateways = CommercePlugin::getInstance()->getGateways()->getAllGateways();
+
+        return $gateways->filter(function(Gateway $gateway) {
+            return $gateway::class == 'craft\commerce\stripe\gateways\PaymentIntents';
+        })->map(function(Gateway $gateway) {
+            return $gateway->id;
+        })->all();
+    }
+
+    /**
+     * @return void
+     * @throws \Throwable
+     */
+    private function _migrateSubscriptionData(): void
+    {
+        $gatewayIds = $this->_stripeCommerceGatewayIds();
+
         // get all subscriptions from commerce
         $commerceSubscriptions = CommerceSubscription::find()
             ->status(null)
+            ->gatewayId(array_values($gatewayIds))
             ->with(['subscriber'])
             ->all();
 
         foreach ($commerceSubscriptions as $commerceSubscription) {
-            $customerId = $commerceSubscription->getSubscriptionData()['customer'];
+            // Check to see if we have already migrated this subscription
+            if (Subscription::find()->stripeId($commerceSubscription->reference)->exists()) {
+                continue;
+            }
 
             // find this subscription in the Stripe data
-            /** @var Subscription $stripeSubscription */
             $stripeSubscription = new Subscription();
             $stripeSubscription->dateCreated = $commerceSubscription->dateCreated;
             $stripeSubscription->stripeId = $commerceSubscription->reference;
