fef811dac01bb03669882109aec3c53c61a77378
i-just <iwona@pixelandtonic.com>
2024-12-09 08:02:54 +0000

    check if apiKey is provided before adding user action menu

--- src/Plugin.php
+++ src/Plugin.php
@@ -572,27 +572,32 @@ class Plugin extends BasePlugin
 
     private function registerUserActions(): void
     {
-        Event::on(
-            User::class,
-            Element::EVENT_DEFINE_ACTION_MENU_ITEMS,
-            function(DefineMenuItemsEvent $event) {
-                $sender = $event->sender;
-                if ($email = $sender->email && Craft::$app->getUser()->checkPermission('accessPlugin-stripe')) {
-                    $customers = Plugin::getInstance()->getApi()->fetchAllCustomers(['email' => $email]);
-                    if ($customers) {
-                        $stripeIds = collect($customers)->pluck('id');
-                        $event->items[] = [
-                            'action' => 'stripe/sync/customer',
-                            'type' => MenuItemType::Button,
-                            'params' => [
-                                'stripeIds' => $stripeIds->toArray(),
-                            ],
-                            'label' => Craft::t('stripe', 'Sync from Stripe'),
-                        ];
+        if (
+            !empty(Plugin::getInstance()->getApi()->getApiKey()) &&
+            Craft::$app->getUser()->checkPermission('accessPlugin-stripe')
+        ) {
+            Event::on(
+                User::class,
+                Element::EVENT_DEFINE_ACTION_MENU_ITEMS,
+                function(DefineMenuItemsEvent $event) {
+                    $sender = $event->sender;
+                    if ($email = $sender->email) {
+                        $customers = Plugin::getInstance()->getApi()->fetchAllCustomers(['email' => $email]);
+                        if ($customers) {
+                            $stripeIds = collect($customers)->pluck('id');
+                            $event->items[] = [
+                                'action' => 'stripe/sync/customer',
+                                'type' => MenuItemType::Button,
+                                'params' => [
+                                    'stripeIds' => $stripeIds->toArray(),
+                                ],
+                                'label' => Craft::t('stripe', 'Sync from Stripe'),
+                            ];
+                        }
                     }
                 }
-            }
-        );
+            );
+        }
     }
 
     /**
