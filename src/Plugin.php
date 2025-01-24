e5a6443f62a8378f0fccb8e83c7cc5fc97dacabe
i-just <iwona@pixelandtonic.com>
2025-01-24 08:06:43 +0000

    show read-only settings when allowAdminChanges is false

--- src/Plugin.php
+++ src/Plugin.php
@@ -97,6 +97,11 @@ class Plugin extends BasePlugin
     /**
      * @inheritdoc
      */
+    public bool $hasReadOnlyCpSettings = true;
+
+    /**
+     * @inheritdoc
+     */
     public bool $hasCpSection = true;
 
     /**
@@ -199,6 +204,14 @@ class Plugin extends BasePlugin
      * @inheritdoc
      */
     public function getSettingsResponse(): mixed
+    {
+        return Craft::$app->getResponse()->redirect(UrlHelper::cpUrl('stripe/settings'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getReadOnlySettingsResponse(): mixed
     {
         return Craft::$app->getResponse()->redirect(UrlHelper::cpUrl('stripe/settings'));
     }
@@ -692,14 +705,12 @@ class Plugin extends BasePlugin
         ];
 
 
-        if (Craft::$app->getUser()->getIsAdmin() && Craft::$app->getConfig()->getGeneral()->allowAdminChanges) {
+        if (Craft::$app->getUser()->getIsAdmin()) {
             $ret['subnav']['stripeSettings'] = [
                 'label' => Craft::t('stripe', 'Settings'),
                 'url' => 'stripe/settings',
             ];
-        }
 
-        if (Craft::$app->getUser()->getIsAdmin()) {
             $ret['subnav']['stripeWebhooks'] = [
                 'label' => Craft::t('stripe', 'Webhooks'),
                 'url' => 'stripe/webhooks',
